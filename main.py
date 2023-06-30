df710ba20ace757e335c42cbcf7f2fab034337da
Burak Türker <pythonu0@gmail.com>
2023-06-30 19:03:50 +0300

    reset,PenUp,PenDown added

--- main.py
+++ main.py
@@ -14,12 +14,21 @@ def turtle_rotate_left():
     turtle_instance.left(10)
 def turtle_backward():
     turtle_instance.backward(20)
+def turtle_Reset():
+    turtle_instance.penup()
+    turtle_instance.home()
+    turtle_instance.clear()
+    turtle_instance.pendown()
+def turtle_pen_up():
+    turtle_instance.penup()
+def turtle_pen_down():
+    turtle_instance.down()
 drawing_board.listen()
 drawing_board.onkey(fun=turtle_forward,key="w")
 drawing_board.onkey(fun=turtle_rotate_right,key="d")
 drawing_board.onkey(fun=turtle_rotate_left,key="a")
 drawing_board.onkey(fun=turtle_backward,key="s")
-drawing_board.onkey(fun=turtle_forward,key="w")
-drawing_board.onkey(fun=turtle_forward,key="w")
-drawing_board.onkey(fun=turtle_forward,key="w")
+drawing_board.onkey(fun=turtle_Reset,key="r")
+drawing_board.onkey(fun=turtle_pen_up,key="Up")
+drawing_board.onkey(fun=turtle_pen_down,key="Down")
 turtle.mainloop()
